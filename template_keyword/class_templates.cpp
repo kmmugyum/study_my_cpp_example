d369225312fe764d4fab199ea3fac97fb8bc7199
kmmugyum <small0753@daum.net>
2023-11-22 15:35:59 +0900

    class templates

--- template_keyword/class_templates.cpp
+++ template_keyword/class_templates.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 
 template <typename T, unsigned int size> // 이런 식으로 만들거다 약간 생성자 느낌도 가지고 있군
+// 사이즈는 컴파일 시간에 상수로 결정되기 때문에 변경이 불가능
 class CustomArray { // template를 통한 선언
 public:
     void print() {
